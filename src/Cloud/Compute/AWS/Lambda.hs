85138a3010d24ad4906fe2dc03040fd2b2c3eacd
Greg Wiley <gwiley@cj.com>
2018-02-13 15:13:59 -0800

    lambdat applicative

--- src/Cloud/Compute/AWS/Lambda.hs
+++ src/Cloud/Compute/AWS/Lambda.hs
@@ -1,6 +1,7 @@
 module Cloud.Compute.AWS.Lambda where
 
 import Data.Functor.Identity(Identity(..), runIdentity)
+import Control.Applicative (liftA2)
 
 type Lambda evt a = LambdaT evt Identity a
 
@@ -13,5 +14,10 @@ liftLambda v = liftLambdaT (pure v)
 newtype LambdaT evt m a = LambdaT { runLambdaT :: evt -> m a }
     deriving (Functor)
 
-liftLambdaT :: (Monad m) =>  m a -> LambdaT evt m a
+liftLambdaT ::  m a -> LambdaT evt m a
 liftLambdaT ma = LambdaT $ const ma
+
+instance (Applicative m) => Applicative (LambdaT evt m) where
+    pure = liftLambdaT . pure
+    LambdaT mf <*> LambdaT ma = LambdaT $ liftA2 (<*>) mf ma
+
--- test/Spec.hs
+++ test/Spec.hs
@@ -4,6 +4,7 @@ import Test.Tasty (TestTree, testGroup, defaultMain)
 import Test.Tasty.HUnit (testCase, (@?=))
 
 import Data.Functor.Identity (Identity(..), runIdentity)
+import Control.Applicative (liftA2)
 
 import Cloud.Compute.AWS.Lambda (runLambdaT, liftLambdaT, runLambda, liftLambda)
 
@@ -18,13 +19,23 @@ tests = testGroup "All Tests" [
             lambda = liftLambdaT (Identity embedded)
         -- then
         runIdentity (runLambdaT lambda "anything") @?= embedded,
-    testCase "functor" $do
+    testCase "functor" $ do
         let embedded = 19
             transform = (+1)
             lambda = liftLambda embedded
         -- when
             actual = transform <$> lambda
         -- then
-        runLambda actual "anything" @?= transform embedded
+        runLambda actual "anything" @?= transform embedded,
+    testCase "applicative" $ do
+        let embedded1 = 19
+            lambda1 = liftLambda embedded1
+            embedded2 = 119
+            lambda2 = liftLambda embedded2
+            op = (+)
+        -- when
+            actual = liftA2 op lambda1 lambda2
+        -- then
+        runLambda actual "anything" @?= op embedded1 embedded2
 
   ]
