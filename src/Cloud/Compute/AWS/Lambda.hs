8c71dfd2902af4f7457a4bb49e8d93161c349c91
Greg Wiley <gwiley@cj.com>
2018-02-17 15:20:59 -0800

    implement with readerT and exceptT

--- src/Cloud/Compute/AWS/Lambda.hs
+++ src/Cloud/Compute/AWS/Lambda.hs
@@ -4,6 +4,9 @@ import Data.Functor.Identity(Identity(..), runIdentity)
 import Control.Applicative (liftA2)
 import Control.Monad.Trans.Class (MonadTrans, lift)
 
+import Control.Monad.Trans.Except
+import Control.Monad.Trans.Reader
+
 type Lambda evt err a = LambdaT evt err Identity a
 
 runLambda :: Lambda evt err a -> evt -> Either err a
@@ -12,30 +15,35 @@ runLambda lambda event = runIdentity (runLambdaT lambda event)
 liftLambda :: a -> Lambda evt err a
 liftLambda v = liftLambdaT (pure v)
 
-newtype LambdaT evt err m a = LambdaT { runLambdaT :: evt -> m (Either err a) }
-    deriving (Functor)
+type LambdaT evt err m = ReaderT evt (ExceptT err m)
 
-liftLambdaT ::  (Functor m) => m a -> LambdaT evt err m a
-liftLambdaT ma = LambdaT $ const (fmap Right ma)
+runLambdaT :: LambdaT evt err m a -> evt -> m (Either err a)
+runLambdaT lambda = runExceptT . runReaderT lambda
 
-argument :: (Applicative m) => LambdaT evt err m evt
-argument = LambdaT $ \e -> pure (Right e)
+-- newtype LambdaT evt err m a = LambdaT { runLambdaT :: evt -> m (Either err a) }
+--     deriving (Functor)
 
-nogood :: (Applicative m) => err -> LambdaT evt err m a
-nogood e = LambdaT $ const (pure (Left e))
+liftLambdaT :: (Monad m) => m a -> LambdaT evt err m a
+liftLambdaT = lift . lift
 
-instance (Applicative m) => Applicative (LambdaT evt err m) where
-    pure = liftLambdaT . pure
-    LambdaT mf <*> LambdaT ma = LambdaT $ (liftA2 . liftA2) (<*>) mf ma
+argument :: (Monad m) => LambdaT evt err m evt
+argument = ask >>= lift . pure
 
--- GeneralizedNewtypeDeriving can't figure out monad
-instance (Monad m) => Monad (LambdaT evt err m) where
-    (LambdaT r) >>= f = LambdaT $ \evt -> do
-        ea <- r evt
-        case ea of
-            Right a -> runLambdaT (f a) evt
-            Left e -> pure (Left e)
+nogood :: (Monad m) => err -> LambdaT evt err m a
+nogood = lift . throwE
 
-instance MonadTrans (LambdaT err evt) where
-    lift = liftLambdaT
+-- instance (Applicative m) => Applicative (LambdaT evt err m) where
+--     pure = liftLambdaT . pure
+--     LambdaT mf <*> LambdaT ma = LambdaT $ (liftA2 . liftA2) (<*>) mf ma
+
+-- -- GeneralizedNewtypeDeriving can't figure out monad
+-- instance (Monad m) => Monad (LambdaT evt err m) where
+--     (LambdaT r) >>= f = LambdaT $ \evt -> do
+--         ea <- r evt
+--         case ea of
+--             Right a -> runLambdaT (f a) evt
+--             Left e -> pure (Left e)
+
+-- instance MonadTrans (LambdaT err evt) where
+--     lift = liftLambdaT
 
