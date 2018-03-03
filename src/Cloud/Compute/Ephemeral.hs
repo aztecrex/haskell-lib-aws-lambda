b4dfe88747c86a5e1faae57920662e8f17fe7ee8
Greg Wiley <gwiley@cj.com>
2018-03-03 11:41:57 -0800

    extract version

--- src/Cloud/Compute/Ephemeral.hs
+++ src/Cloud/Compute/Ephemeral.hs
@@ -19,6 +19,7 @@ class MonadEphemeralTimer m where
 
 class EphemeralInfo a where
     functionName :: a -> Text
+    functionVersion :: a -> Text
 
 -- class EphemeralTimer a where
 --     functionTimeLeft :: a -> UTCTime -> DiffTime
@@ -26,6 +27,6 @@ class EphemeralInfo a where
 
 instance (Monad m, EphemeralInfo ctx) => MonadEphemeralInfo (ComputeT ctx evt err m) where
     name = functionName <$> context
-    version = error "not implemented"
+    version = functionVersion <$> context
     invocation = error "not implemented"
 
--- test/Spec/Cloud/Compute/Ephemeral.hs
+++ test/Spec/Cloud/Compute/Ephemeral.hs
@@ -7,19 +7,27 @@ import Spec.TestHelp ((@?>=))
 import Data.Text (Text)
 
 import Cloud.Compute (runCompute)
-import Cloud.Compute.Ephemeral (EphemeralInfo (..), name)
+import Cloud.Compute.Ephemeral (EphemeralInfo (..), name, version)
 
 tests :: TestTree
 tests = testGroup "Ephemeral" [
-        testCase "extract name from context" $ do
+        testCase "extract name" $ do
             -- given
             let namev = "function name"
                 ctx = Name namev
             -- when
                 actual = name
             -- then
-            runCompute actual ctx "any" @?>= namev
+            runCompute actual ctx "any" @?>= namev,
 
+        testCase "extract version" $ do
+            -- given
+            let versionv = "function version"
+                ctx = Version versionv
+            -- when
+                actual = version
+            -- then
+            runCompute actual ctx "any" @?>= versionv
     ]
 
 
@@ -27,4 +35,11 @@ newtype Name = Name { unname :: Text } deriving (Eq, Show)
 
 instance EphemeralInfo Name where
     functionName = unname
+    functionVersion = error "not implemented"
+
+newtype Version = Version { unversion :: Text } deriving (Eq, Show)
+
+instance EphemeralInfo Version where
+    functionVersion = unversion
+    functionName = error "not implemented"
 
